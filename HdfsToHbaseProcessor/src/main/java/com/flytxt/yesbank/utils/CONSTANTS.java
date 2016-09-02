c1018d8bbe6ecab0ebf08cef94f128533422cf90
Deepak <deepak.k@flytxt.com>
2016-09-02 21:58:27 +0530

    Hdfs to Hbase Processor License Files License Files

--- HdfsToHbaseProcessor/src/main/java/com/flytxt/yesbank/utils/CONSTANTS.java
+++ HdfsToHbaseProcessor/src/main/java/com/flytxt/yesbank/utils/CONSTANTS.java
@@ -1,3 +1,17 @@
+/*Copyright (c) 02-Sep-2016 FlyTxt Corporation and others.
+All rights reserved. This program and the accompanying materials
+are made available under the terms of the Eclipse Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/epl-v10.html
+
+Contributors:
+    Deepak  K- initial API and implementation
+    
+Author : Deepak K
+date : 02-Sep-2016
+
+*/
+
 package com.flytxt.yesbank.utils;
 
 public class CONSTANTS {
--- HdfsToHbaseProcessor/src/test/java/com/flytxt/yesbank/test/ModelProcessor.java
+++ HdfsToHbaseProcessor/src/test/java/com/flytxt/yesbank/test/ModelProcessor.java
@@ -1,8 +1,18 @@
-package com.flytxt.yesbank.test;
-/* Author : Deepak K
-27-Aug-2016 12:49:24 PM 
-fly
+/*Copyright (c) 02-Sep-2016 FlyTxt Corporation and others.
+All rights reserved. This program and the accompanying materials
+are made available under the terms of the Eclipse Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/epl-v10.html
+
+Contributors:
+    Deepak  K- initial API and implementation
+    
+Author : Deepak K
+date : 02-Sep-2016
+
 */
+
+package com.flytxt.yesbank.test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
